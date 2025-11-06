dd049aa643e4d5b33a1ded3ef73f2048eb064243
Adnan- <117507373+ADNAN000000000@users.noreply.github.com>
2025-11-06 06:16:30 +0530

    Added basics of js

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 141414
+let accountMail = "adnan@gmail.com"
+var accountPassword = "123123"
+accountCity ="Hyderabad"
+let accountState;
+
+
+ accountMail = "ad@gmail.com"
+ accountPassword = "1223"
+accountCity ="Bengalore"
+// accountId = 123 // thsi is not allowed since we are using const so the values can't be changed
+console.log(accountId)
+console.table([accountId, accountMail, accountPassword, accountCity, accountState])
